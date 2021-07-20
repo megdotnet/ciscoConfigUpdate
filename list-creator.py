bcc0be819c4e4c0491f19cef3b65053f06ac84c8
Meghan Fairbanks <meghan@fairbanks.ink>
2021-07-20 16:51:02 -0500

    Change how logging is done

--- list-creator.py
+++ list-creator.py
@@ -6,16 +6,13 @@ import os, re, shutil
 search= "ip address 10.11."
 folder = "TCORE"
 
-path = ('D:\\OneDrive - Tacocat\\Code\\git\\ciscoConfigUpdate\\' + folder)
-outpath1 = path + "\\ip_list.txt"
-outpath2 = path + "\\ip_hostname_list.txt"
+path = ('C:\\users\\fairbanksm\\OneDrive - Tacocat\\Code\\git\\ciscoConfigUpdate\\' + folder)
+outpath = path + "\\ip_list.txt"
 
 os.chdir(path)
 
-file1 = open(outpath1, "w")
-file2 = open(outpath2, "w")
-file1.close()
-file2.close()
+file = open(outpath, "w")
+file.close()
 
 #find the first ip address that mathes search and stop
 def parse(fullname):
@@ -26,12 +23,9 @@ def parse(fullname):
                 hostname = line.split(" ")[1]
             if search in line.strip():
                 nat_rules = line.split(" ")[3]
-                file1 = open(outpath1, "a")
-                file1.write(nat_rules + "\n")
-                file1.close()
-                file2 = open(outpath2, "a")
-                file2.write(nat_rules + " " + hostname + "\n")
-                file2.close()
+                file = open(outpath, "a")
+                file.write(nat_rules + " " + hostname + "\n")
+                file.close()
                 break
 
 #read each file in the directory
